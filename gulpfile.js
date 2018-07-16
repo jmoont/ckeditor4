b348b9860f4fdfbaf613f51e70aee4f75ed5a38f
Josh Moont <moonty@gmail.com>
2018-07-16 11:47:39 +0100

    add all plugins

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,9 @@ gulp.task('ckeditor', function() {
         ckeditorPath+'/styles.js', 
         ckeditorPath+'/lang/en-gb.js', 
         ckeditorPath+'/skins/moono-lisa/*',
-        ckeditorPath+'/plugins/tableselection/styles/tableselection.css',
-        ckeditorPath+'/plugins/scayt/skins/moono-lisa/scayt.css',
-        ckeditorPath+'/plugins/scayt/dialogs/dialog.css',
-        ckeditorPath+'/plugins/wsc/skins/moono-lisa/wsc.css',
+        ckeditorPath+'/plugins/**/*',
+        '!'+ckeditorPath+'/plugins/**/lang/!(en-gb.js)',
         ckeditorPath+'/contents.css',
-        ckeditorPath+'/plugins/tableselection/styles/tableselection.css'
     ],  {base: ckeditorPath})
     .pipe(gulp.dest(libPath+'/ckeditor/dist'));
 });
