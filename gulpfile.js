b0adbddc06f26d0e2af9aded676a4326644a0655
Josh Moont <moonty@gmail.com>
2018-07-16 11:31:06 +0100

    added missing styles

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,17 @@ gulp.task('ckeditor', function() {
         ckeditorPath+'/config.js', 
         ckeditorPath+'/styles.js', 
         ckeditorPath+'/lang/en-gb.js', 
-        ckeditorPath+'/skins/moono-lisa/*'
+        ckeditorPath+'/skins/moono-lisa/*',
+        ckeditorPath+'/plugins/tableselection/styles/tableselection.css',
+        ckeditorPath+'/plugins/scayt/skins/moono-lisa/scayt.css',
+        ckeditorPath+'/plugins/scayt/dialogs/dialog.css',
+        ckeditorPath+'/plugins/wsc/skins/moono-lisa/wsc.css',
+        ckeditorPath+'/contents.css',
+        ckeditorPath+'/plugins/tableselection/styles/tableselection.css'
     ],  {base: ckeditorPath})
     .pipe(gulp.dest(libPath+'/ckeditor/dist'));
 });
 
 gulp.task('default', ['ckeditor']);
+
+
