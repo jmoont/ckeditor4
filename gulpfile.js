654b37acb7ebf02b34ec6c0c98ba148c921dc56b
Josh Moont <moonty@gmail.com>
2018-07-16 11:27:01 +0100

    add styles.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,14 @@ var ckeditorPath = 'node_modules/ckeditor';
 var libPath = 'lib';
 
 gulp.task('ckeditor', function() {
-    return gulp.src([ckeditorPath+'/ckeditor.js', ckeditorPath+'/config.js', ckeditorPath+'/lang/en-gb.js', ckeditorPath+'/skins/moono-lisa/*'],  {base: ckeditorPath})
-        .pipe(gulp.dest(libPath+'/ckeditor/dist'));
+    return gulp.src([
+        ckeditorPath+'/ckeditor.js', 
+        ckeditorPath+'/config.js', 
+        ckeditorPath+'/styles.js', 
+        ckeditorPath+'/lang/en-gb.js', 
+        ckeditorPath+'/skins/moono-lisa/*'
+    ],  {base: ckeditorPath})
+    .pipe(gulp.dest(libPath+'/ckeditor/dist'));
 });
 
 gulp.task('default', ['ckeditor']);
